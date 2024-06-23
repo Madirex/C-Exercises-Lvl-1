7e8fbe08b6bc858e48c0f410c05291247b651c7f
Madirex <angel@madirex.com>
2024-06-23 20:05:25 +0200

    added ulstr

---
+++ ulstr/ulstr.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+int main(int argn, char **argv)
+{
+    int i = 0;
+
+    if (argn == 2)
+    {
+        char c = argv[1][i];
+
+        while(c)
+        {
+            if (c >= 'a' && c <= 'z')
+                c -= 32;
+            else if (c >= 'A' && c <= 'Z')
+                c += 32;
+            write(1, &c, 1);
+            i++;
+            c = argv[1][i];
+        }
+    }
+
+    write(1, "\n", 1);
+    return (0);
+}
