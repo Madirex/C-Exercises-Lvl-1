ce1f645ba4c2dd99d42c6fe78b5e5d8ddb2171ca
Madirex <angel@madirex.com>
2024-06-23 19:48:49 +0200

    added rotone

---
+++ rotone/rotone.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+int main(int argn, char **argc)
+{
+    int i = 0;
+
+    if (argn == 2)
+    {
+        char c = argc[1][i];
+        while(c)
+        {
+            if (c >= 'a' && c <= 'z')
+                c = (c - 'a' + 1) % 26 + 'a';
+            else if (c >= 'A' && c <= 'Z')
+                c = (c - 'A' + 1) % 26 + 'A';
+            write(1, &c, 1);
+            i++;
+            c = argc[1][i];
+        }
+    }
+
+    write(1, "\n", 1);
+    return (0);
+}
